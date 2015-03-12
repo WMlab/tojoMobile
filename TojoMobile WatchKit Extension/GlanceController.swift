ca18f2c6bd5ef8953d31c2b13890044ca2023c4d
sdq <sdq1990@outlook.com>
2015-03-12 10:55:31 +0800

    add watch extension

---
+++ TojoMobile WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  TojoMobile WatchKit Extension
+//
+//  Created by sdq on 15/3/12.
+//  Copyright (c) 2015年 Tongjo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
