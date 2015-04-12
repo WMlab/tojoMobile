3cea5bec122e6cde1324150ab187ec6c9667363c
sdq <sdq1990@outlook.com>
2015-04-12 11:35:01 +0800

    查看完整项目介绍页面完成

--- TojoMobile/projects/TJProjectInfoViewController.h
+++ TojoMobile/projects/TJProjectInfoViewController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "TJProjectInfoModel.h"
 
 @interface TJProjectInfoViewController : UIViewController<UITableViewDataSource,UITableViewDelegate>
+
+-(void)setInfoModel:(TJProjectInfoModel *)infoModel;
 
 @end
